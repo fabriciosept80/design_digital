9e3d8b7fba43f8223574b772be1e538ff60040f9
Fabricio <fabriciocmh@outlook.com>
2021-11-14 13:39:07 -0300

    corrigindo comit posterior

---
+++ src/script.js
@@ -0,0 +1,41 @@
+/*$(document).ready(function () {
+   /* $(window).scroll(function () {
+        if (this.srolly > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+});*/
+$(document).ready(function () {
+
+    $('.carousel').owlCarousel({
+        margin:30,
+        loop:true,
+        zIndex:999,
+
+        autoplayTimeOut:100,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            800:{
+                items:2,
+                nav:false
+            },
+            3000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+});
+const btnMobile = document.getElementById('btn-mobile');
+function toggleMenu(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+
+}
+btnMobile.addEventListener('click',toggleMenu);
